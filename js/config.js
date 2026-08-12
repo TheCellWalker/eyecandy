4ed98be8e106690e8857a3a9a89cc179b720cdae
TheCellWalker <CellWalker@proton.me>
2026-08-12 15:26:20 -0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,1 @@
+window.FB_CONFIG={apiKey:"AIza...",authDomain:"YOUR-PROJECT.firebaseapp.com",projectId:"YOUR-PROJECT",storageBucket:"YOUR-PROJECT.appspot.com",messagingSenderId:"0",appId:"1:0:web:0"};
